43dd6e4d26c5069b6bef77ceb4f197440c58d5ad
Sámuel Fekete <feketesamu@gmail.com>
2021-07-21 13:34:54 +0200

    reducers and detools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reduxThunk from 'redux-thunk'
 
 import combinedReducer from './reducers'
 import App from './components/App'
 
-const store = createStore(combinedReducer, applyMiddleware(reduxThunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(combinedReducer, composeEnhancers(applyMiddleware(reduxThunk)))
 
 ReactDOM.render(
     <Provider store={store}>
--- src/reducers/songsReducer.js
+++ src/reducers/songsReducer.js
@@ -1,11 +1,19 @@
 import _ from 'lodash'
-import { FETCH_SONGS } from "../actions/types"
+import { FETCH_SONGS, FETCH_SONG, CREATE_SONG, EDIT_SONG, DELETE_SONG } from "../actions/types"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = {}, action) => {
     switch (action.type) {
         case FETCH_SONGS:
             return {...state, ..._.mapKeys(action.payload, 'id')}
+        case FETCH_SONG:
+            return {...state, [action.payload.id]: action.payload}
+        case CREATE_SONG:
+            return {...state, [action.payload.id]: action.payload}
+        case EDIT_SONG:
+            return {...state, [action.payload.id]: action.payload}
+        case DELETE_SONG:
+            return _.omit(state, action.payload)
         default:
             return state
     }
