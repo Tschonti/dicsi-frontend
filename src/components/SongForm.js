998bb476ec40bdc0c22f4283526fa99fd075b313
Sámuel Fekete <feketesamu@gmail.com>
2021-07-21 17:35:52 +0200

    form without bugs

--- src/components/SongForm.js
+++ src/components/SongForm.js
@@ -1,49 +1,49 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
+const renderInput = ({ input, label, meta, type }) => {
+    return (
+        <div className={`field ${meta.error && meta.touched ? 'error' : ''}`}>
+            <label>{label}</label>
+            <input {...input} type={type} autoComplete="off"/>
+            {renderError(meta)}
+        </div>
+    )
+}
+
+const renderTextArea = ({ input, label, meta }) => {
+    return (
+        <div className={`field ${meta.error && meta.touched ? 'error' : ''}`}>
+            <label>{label}</label>
+            <textarea {...input} autoComplete="off"/>
+            {renderError(meta)}
+        </div>
+    )
+}
+
+const renderError = ({ error, touched }) => {
+    if (touched && error) {
+        return (
+            <div className="ui error message">
+                <div className="header">
+                    {error}
+                </div>
+            </div>
+        )
+    }
+}
+
 const SongForm = props => {
 
     const onSubmit = formValues => {
         props.onSubmit(formValues)
     }
 
-    const renderError = ({ error, touched }) => {
-        if (touched && error) {
-            return (
-                <div className="ui error message">
-                    <div className="header">
-                        {error}
-                    </div>
-                </div>
-            )
-        }
-    }
-
-    const renderInput = ({ input, label, meta, type }) => {
-        return (
-            <div className={`field ${meta.error && meta.touched ? 'error' : ''}`}>
-                <label>{label}</label>
-                <input {...input} type={type} autoComplete="off"/>
-                {renderError(meta)}
-            </div>
-        )
-    }
-
-    const renderTextArea = ({ input, label, meta }) => {
-        return (
-            <div className={`field ${meta.error && meta.touched ? 'error' : ''}`}>
-                <label>{label}</label>
-                <textarea {...input} autoComplete="off"/>
-                {renderError(meta)}
-            </div>
-        )
-    }
-
     return (
         <form className="ui form error" onSubmit={props.handleSubmit(onSubmit)}>
             <Field name="id" component={renderInput} label="Sorszám" type="number"/>
             <Field name="title" component={renderInput} label="Cím" type="text"/>
-            <Field name="lyrics" component={renderTextArea} label="Dalszöveg" type="text"/>
+            <Field name="lyrics" component={renderTextArea} label="Dalszöveg"/>
             <Field name="pwd" component={renderInput} label="Jelszó" type="password"/>
             <button className="ui button primary">Mentés</button>
         </form>
--- src/components/SongShow.js
+++ src/components/SongShow.js
@@ -5,6 +5,18 @@ import { Link } from 'react-router-dom'
 import { fetchSong } from '../actions'
 
 class SongShow extends React.Component {
+    state = {
+        fontSize: 14
+    }
+
+    onSizeChange = (increase) => {
+        if (increase && this.state.fontSize < 70) {
+            this.setState({fontSize: this.state.fontSize + 3})
+        } else if (!increase && this.state.fontSize > 8) {
+            this.setState({fontSize: this.state.fontSize - 3})
+        }
+    }
+
     componentDidMount() {
         this.props.fetchSong(this.props.match.params.id)
     }
@@ -12,14 +24,18 @@ class SongShow extends React.Component {
     render() {
         if (this.props.song) {
             const verses = this.props.song.verses.map((verse, idx) =>{
-                const lines = verse.split('\n').map(line => <React.Fragment>{line}<br/></React.Fragment>)
-                return <p key={idx}>{lines}</p>
+                const lines = verse.split('\n').map((line, idx) => <React.Fragment key={idx}>{line}<br/></React.Fragment>)
+                return <p style={{fontSize: `${this.state.fontSize}px`}} key={idx}>{lines}</p>
             } )
             return (
                 <div className="ui container">
                     <h2>{this.props.song.id}. {this.props.song.title}</h2>
-                    {verses}
                     <Link to={`/songs/edit/${this.props.match.params.id}`}>Dicsi szerkeztése</Link>
+                    <button className="ui button primary" onClick={() => this.onSizeChange(false)}><i class="font icon"></i><i class="arrow down icon"></i></button>
+                    <button className="ui button primary" onClick={() => this.onSizeChange(true)}><i class="font icon"></i><i class="arrow up icon"></i></button>
+                    <button className="ui button primary" onClick={() => this.setState({ fontSize: 14 })}><i class="font icon"></i><i class="undo icon"></i></button>
+
+                    {verses}
                 </div>
             )
         }
