dc371ec8be4838535bc7163f203900029ad03cba
Sámuel Fekete <feketesamu@gmail.com>
2021-07-23 21:51:09 +0200

    localstorage

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import reduxThunk from 'redux-thunk'
+import { save, load } from "redux-localstorage-simple"
 
 import combinedReducer from './reducers'
 import App from './components/App'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(combinedReducer, composeEnhancers(applyMiddleware(reduxThunk)))
+//const store = createStore(combinedReducer, composeEnhancers(applyMiddleware(reduxThunk)))
+
+const createStoreWithMiddleware  =composeEnhancers(applyMiddleware(save({ states: ["playlist"], debounce: 500}), reduxThunk))(createStore)
+const store = createStoreWithMiddleware(combinedReducer, load({ states: ["playlist"]}))
 
 ReactDOM.render(
     <Provider store={store}>
