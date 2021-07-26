020f1911d912aca22a814b7e4d570e82f3078fd5
Sámuel Fekete <feketesamu@gmail.com>
2021-07-26 17:54:01 +0200

    mobile header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Alert from '@material-ui/lab/Alert'
 import { connect } from 'react-redux'
+import { isMobileOnly } from 'react-device-detect'
 
 import '../styles.css'
 import { removeAlert } from '../actions'
@@ -13,9 +14,10 @@ const Header = (props) => {
             {props.alert.msg}
         </Alert>
         : null
+    const title = isMobileOnly ? <div className="big-text-nom">ÖKGY Énekeskönyv</div> : <h2>ÖKGY Énekeskönyv</h2>
     return (
         <div className="ui secondary pointing menu my-header">
-            <Link to="/dicsi" className="item "><h2>ÖKGY Énekeskönyv</h2></Link>
+            <Link to="/dicsi" className="item ">{title}</Link>
             <Link to="/dicsi" className="item header-link">Énekek listája</Link>
             <Link to="/dicsi/songs/new" className="item header-link">Új ének</Link>
             <div className="centered">
